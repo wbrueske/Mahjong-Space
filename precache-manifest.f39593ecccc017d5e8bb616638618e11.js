ffb2e299243562abe6919e6b811278a2b64507ee
Will Brueske <wbrueske@live.com>
2019-12-10 22:00:52 -0600

    Updates

---
+++ precache-manifest.f39593ecccc017d5e8bb616638618e11.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658972aad1bceb42b4641aeaf16a48fd",
+    "url": "/mahjong-space/index.html"
+  },
+  {
+    "revision": "ac15fc13373c995d4d1b",
+    "url": "/mahjong-space/static/css/main.e9172a54.chunk.css"
+  },
+  {
+    "revision": "2fd01ab2b37b3fbe7828",
+    "url": "/mahjong-space/static/js/2.f523dc61.chunk.js"
+  },
+  {
+    "revision": "ac15fc13373c995d4d1b",
+    "url": "/mahjong-space/static/js/main.8ececdaa.chunk.js"
+  },
+  {
+    "revision": "22d7d2182c55ae2c3db4",
+    "url": "/mahjong-space/static/js/runtime-main.de18c23e.js"
+  },
+  {
+    "revision": "0dd37be6308079fdd5a5fc20a519f2fd",
+    "url": "/mahjong-space/static/media/bg-1024.0dd37be6.jpg"
+  },
+  {
+    "revision": "4b33b9c4a1fc8513d188dbb85afffc93",
+    "url": "/mahjong-space/static/media/bg-2048.4b33b9c4.jpg"
+  },
+  {
+    "revision": "3069bdf9dfe1461f982d78f288d915f6",
+    "url": "/mahjong-space/static/media/bg-4096.3069bdf9.jpg"
+  },
+  {
+    "revision": "f1a4acc6591553df448f0fbf292fcb67",
+    "url": "/mahjong-space/static/media/tilesheet-high.f1a4acc6.png"
+  },
+  {
+    "revision": "f0926599eae04d1ba4bb2f46010d0c1f",
+    "url": "/mahjong-space/static/media/tilesheet-low.f0926599.png"
+  },
+  {
+    "revision": "e9cdd96548e6639b34b05186443fe032",
+    "url": "/mahjong-space/static/media/tilesheet-medium.e9cdd965.png"
+  },
+  {
+    "revision": "d57166bb3d5dc2529c84a7c40a42bc00",
+    "url": "/mahjong-space/static/media/tilesheet-ultra.d57166bb.png"
+  },
+  {
+    "revision": "09c5f9a7a9c58b8431daddade61ed2bf",
+    "url": "/mahjong-space/static/media/tilesheet-veryhigh.09c5f9a7.png"
+  }
+]);
